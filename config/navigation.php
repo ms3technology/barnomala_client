74cd307cbc55b2d98c6c7ddcf39238ceba313caf
mushfiqbh <mushfiqbh@gmail.com>
2026-04-07 14:11:58 +0600

    Chore: Project Config Files Setup

---
+++ config/navigation.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'navigation_items' => [
+        ['label' => 'Home', 'route' => 'home', 'children' => []],
+        ['label' => 'About', 'url' => '#', 'children' => [
+            ['label' => 'Speech', 'route' => 'speeches.index'],
+            ['label' => 'History', 'route' => 'history.index'],
+            ['label' => 'Achivements', 'route' => 'achievements.index'],
+        ]],
+        ['label' => 'Academic', 'route' => 'academic.index', 'children' => [
+            ['label' => 'Academic Calendar', 'route' => 'home'],
+            ['label' => 'Academic Rules', 'route' => 'home'],
+            ['label' => 'Class Schedule', 'route' => 'home'],
+            ['label' => 'Teachers', 'route' => 'home'],
+            ['label' => 'Former Teachers', 'route' => 'home'],
+            ['label' => 'Lecturers', 'route' => 'home'],
+            ['label' => 'Staff', 'route' => 'home'],
+            ['label' => 'Former Staff', 'route' => 'home'],
+            ['label' => 'Committees', 'route' => 'home'],
+        ]],
+        ['label' => 'Student', 'route' => 'teachers.index', 'children' => [
+            ['label' => 'Tution Fees', 'route' => 'home'],
+            ['label' => 'Our Student', 'route' => 'home'],
+            ['label' => 'Student Uniform', 'route' => 'home'],
+            ['label' => 'Exam Schedule', 'route' => 'home'],
+            ['label' => 'Daily Activities', 'route' => 'home'],
+            ['label' => 'Mobile Banking', 'route' => 'home'],
+        ]],
+        ['label' => 'Result', 'route' => 'results.index', 'children' => []],
+        ['label' => 'Notice', 'route' => 'news.index', 'children' => []],
+        ['label' => 'News', 'route' => 'news.index', 'children' => []],
+        ['label' => 'Gallery', 'route' => 'news.index', 'children' => []],
+        ['label' => 'Contact', 'route' => 'news.index', 'children' => []],
+        ['label' => 'Apply Online', 'route' => 'apply.index', 'children' => []],
+    ],
+
+    'important_links' => [
+        ['title' => 'Sylhet Education Board', 'url' => 'https://www.sylhetboard.gov.bd'],
+        ['title' => 'Directorate of Secondary and Higher Education', 'url' => 'https://www.dshe.gov.bd'],
+        ['title' => 'Ministry of Education', 'url' => 'https://moedu.gov.bd'],
+        ['title' => 'Bangladesh National Portal', 'url' => 'https://bangladesh.gov.bd'],
+        ['title' => 'BANBEIS', 'url' => 'https://www.banbeis.gov.bd'],
+        ['title' => 'Teachers Portal', 'url' => 'https://www.teachers.gov.bd'],
+    ],
+];
